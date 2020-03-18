cc9d82d89a10ec80896e14839df616902e70e711
AvBorikDeveloper <avboris28@gmail.com>
2020-03-19 07:14:58 +1300

    Added the move back seek bar, Added the alerts, Implemented the seek bar methods

--- app/src/main/java/com/avborik28/integratedaudioandvideo/MainActivity.java
+++ app/src/main/java/com/avborik28/integratedaudioandvideo/MainActivity.java
@@ -2,15 +2,18 @@ package com.avborik28.integratedaudioandvideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private VideoView myVideoView;
     private Button btnPlayVideo;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnPauseMusic;
     private MediaController mediaController;
     private MediaPlayer mediaPlayer;
+    private SeekBar seekBarVolume;
+    private AudioManager audioManger;
+    private SeekBar moveBackAndForthSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +34,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlayVideo = findViewById(R.id.btnPlayVideo);
         btnPlayMusic = findViewById(R.id.btnPlayMusic);
         btnPauseMusic = findViewById(R.id.btnPauseMusic);
-
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+        moveBackAndForthSeekBar = findViewById(R.id.seekBarMove);
 
         btnPlayVideo.setOnClickListener(MainActivity.this);
         btnPlayMusic.setOnClickListener(MainActivity.this);
         btnPauseMusic.setOnClickListener(MainActivity.this);
 
         mediaController = new MediaController(MainActivity.this);
-
         mediaPlayer = MediaPlayer.create(this, R.raw.android_music);
+        audioManger = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maximumVolumeOfUserDevice = audioManger.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolumeOfUserDevice = audioManger.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekBarVolume.setMax(maximumVolumeOfUserDevice);
+        seekBarVolume.setProgress(currentVolumeOfUserDevice);
+
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    Toast.makeText(MainActivity.this, Integer.toString(progress),Toast.LENGTH_LONG).show();
+                    audioManger.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        moveBackAndForthSeekBar.setOnSeekBarChangeListener(this);
+
+    }
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(fromUser){
+            Toast.makeText(this, Integer.toString(progress), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 
@@ -62,6 +113,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             break;
         }
 
-
     }
 }
+
